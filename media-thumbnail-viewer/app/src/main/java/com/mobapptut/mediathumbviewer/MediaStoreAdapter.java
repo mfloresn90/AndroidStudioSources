ba5ed393a452082b7f9769bb5e29a21341bb45fd
Mauricio Flores Nicolás <yoshiblack90@gmail.com>
2017-07-21 15:07:31 -0500

    More android code samples.

---
+++ media-thumbnail-viewer/app/src/main/java/com/mobapptut/mediathumbviewer/MediaStoreAdapter.java
@@ -0,0 +1,155 @@
+package com.mobapptut.mediathumbviewer;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by nigelhenshaw on 1/06/2016.
+ */
+public class MediaStoreAdapter extends RecyclerView.Adapter<MediaStoreAdapter.ViewHolder> {
+
+    private Cursor mMediaStoreCursor;
+    private final Activity mActivity;
+    private OnClickThumbListener mOnClickThumbListener;
+
+    public interface OnClickThumbListener {
+        void OnClickImage(Uri imageUri);
+        void OnClickVideo(Uri videoUri);
+    }
+    public MediaStoreAdapter(Activity activity) {
+        this.mActivity = activity;
+        this.mOnClickThumbListener = (OnClickThumbListener)activity;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.media_image_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        /*
+        Bitmap bitmap = getBitmapFromMediaStore(position);
+        if (bitmap != null) {
+            holder.getImageView().setImageBitmap(bitmap);
+        }
+        */
+        Glide.with(mActivity)
+                .load(getUriFromMediaStore(position))
+                .centerCrop()
+                .override(96, 96)
+                .into(holder.getImageView());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (mMediaStoreCursor == null) ? 0 : mMediaStoreCursor.getCount();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private final ImageView mImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mImageView = (ImageView) itemView.findViewById(R.id.mediastoreImageView);
+            mImageView.setOnClickListener(this);
+        }
+
+        public ImageView getImageView() {
+            return mImageView;
+        }
+
+        @Override
+        public void onClick(View v) {
+            getOnClickUri(getAdapterPosition());
+        }
+    }
+
+    private Cursor swapCursor(Cursor cursor) {
+        if (mMediaStoreCursor == cursor) {
+            return null;
+        }
+        Cursor oldCursor = mMediaStoreCursor;
+        this.mMediaStoreCursor = cursor;
+        if (cursor != null) {
+            this.notifyDataSetChanged();
+        }
+        return oldCursor;
+    }
+
+    public void changeCursor(Cursor cursor) {
+        Cursor oldCursor = swapCursor(cursor);
+        if (oldCursor != null) {
+            oldCursor.close();
+        }
+    }
+
+    private Bitmap getBitmapFromMediaStore(int position) {
+        int idIndex = mMediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns._ID);
+        int mediaTypeIndex = mMediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE);
+
+        mMediaStoreCursor.moveToPosition(position);
+        switch (mMediaStoreCursor.getInt(mediaTypeIndex)) {
+            case MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE:
+                return MediaStore.Images.Thumbnails.getThumbnail(
+                        mActivity.getContentResolver(),
+                        mMediaStoreCursor.getLong(idIndex),
+                        MediaStore.Images.Thumbnails.MICRO_KIND,
+                        null
+                );
+            case MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO:
+                return MediaStore.Video.Thumbnails.getThumbnail(
+                    mActivity.getContentResolver(),
+                        mMediaStoreCursor.getLong(idIndex),
+                        MediaStore.Video.Thumbnails.MICRO_KIND,
+                        null
+                );
+            default:
+                return null;
+        }
+    }
+
+    private Uri getUriFromMediaStore(int position) {
+        int dataIndex = mMediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns.DATA);
+
+        mMediaStoreCursor.moveToPosition(position);
+
+        String dataString = mMediaStoreCursor.getString(dataIndex);
+        Uri mediaUri = Uri.parse("file://" + dataString);
+        return mediaUri;
+    }
+
+    private void getOnClickUri(int position) {
+        int mediaTypeIndex = mMediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE);
+        int dataIndex = mMediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns.DATA);
+
+        mMediaStoreCursor.moveToPosition(position);
+        String dataString = mMediaStoreCursor.getString(dataIndex);
+        Uri mediaUri = Uri.parse("file://" + dataString);
+
+        switch (mMediaStoreCursor.getInt(mediaTypeIndex)) {
+            case MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE:
+                mOnClickThumbListener.OnClickImage(mediaUri);
+                break;
+            case MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO:
+                mOnClickThumbListener.OnClickVideo(mediaUri);
+                break;
+            default:
+        }
+
+    }
+}
---
+++ media-thumbnail-viewer/app/src/main/java/com/mobapptut/mediathumbviewer/MediaThumbMainActivity.java
@@ -0,0 +1,125 @@
+package com.mobapptut.mediathumbviewer;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+public class MediaThumbMainActivity extends AppCompatActivity
+    implements LoaderManager.LoaderCallbacks<Cursor>, MediaStoreAdapter.OnClickThumbListener {
+
+    private final static int READ_EXTERNAL_STORAGE_PERMMISSION_RESULT = 0;
+    private final static int MEDIASTORE_LOADER_ID = 0;
+    private RecyclerView mThumbnailRecyclerView;
+    private MediaStoreAdapter mMediaStoreAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_thumb_main);
+
+        mThumbnailRecyclerView = (RecyclerView) findViewById(R.id.thumbnailRecyclerView);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        mThumbnailRecyclerView.setLayoutManager(gridLayoutManager);
+        mMediaStoreAdapter = new MediaStoreAdapter(this);
+        mThumbnailRecyclerView.setAdapter(mMediaStoreAdapter);
+
+        checkReadExternalStoragePermission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case READ_EXTERNAL_STORAGE_PERMMISSION_RESULT:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Call cursor loader
+                    // Toast.makeText(this, "Now have access to view thumbs", Toast.LENGTH_SHORT).show();
+                    getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null, this);
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void checkReadExternalStoragePermission() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                // Start cursor loader
+                getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null, this);
+            } else {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "App needs to view thumbnails", Toast.LENGTH_SHORT).show();
+                }
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                        READ_EXTERNAL_STORAGE_PERMMISSION_RESULT);
+            }
+        } else {
+            // Start cursor loader
+            getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null, this);
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {
+                MediaStore.Files.FileColumns._ID,
+                MediaStore.Files.FileColumns.DATE_ADDED,
+                MediaStore.Files.FileColumns.DATA,
+                MediaStore.Files.FileColumns.MEDIA_TYPE
+        };
+        String selection = MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
+                + " OR "
+                + MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+        return new CursorLoader(
+                this,
+                MediaStore.Files.getContentUri("external"),
+                projection,
+                selection,
+                null,
+                MediaStore.Files.FileColumns.DATE_ADDED + " DESC"
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mMediaStoreAdapter.changeCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mMediaStoreAdapter.changeCursor(null);
+    }
+
+    @Override
+    public void OnClickImage(Uri imageUri) {
+        // Toast.makeText(MediaThumbMainActivity.this, "Image uri = " + imageUri.toString(), Toast.LENGTH_SHORT).show();
+        Intent fullScreenIntent = new Intent(this, FullScreenImageActivity.class);
+        fullScreenIntent.setData(imageUri);
+        startActivity(fullScreenIntent);
+    }
+
+    @Override
+    public void OnClickVideo(Uri videoUri) {
+        Intent videoPlayIntent = new Intent(this, VideoPlayActivity.class);
+        videoPlayIntent.setData(videoUri);
+        startActivity(videoPlayIntent);
+
+    }
+}
